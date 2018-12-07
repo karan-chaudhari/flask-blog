c4c31a5699d252eec775805f213cc56b110fd8d8
Karan Chaudhari <chaudharik353@gmail.com>
2018-12-07 22:50:13 +0530

    send contacts to database

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:@localhost/code blogger"
+db = SQLAlchemy(app)
+
+class Contacts(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=False, nullable=False)
+    email = db.Column(db.String(50), unique=False, nullable=False)
+    phone_num = db.Column(db.String(15), unique=False, nullable=False)
+    message = db.Column(db.String(500), unique=False, nullable=False)
+    date = db.Column(db.String(12), unique=False, nullable=True)
 
 @app.route("/")
 def home():
@@ -10,8 +22,16 @@ def home():
 def about():
     return render_template("about.html")
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET","POST"])
 def contact():
+    if request.method == "POST":
+        name = request.form.get("name")
+        email = request.form.get("email")
+        phone = request.form.get("phone")
+        msg = request.form.get("message")
+        entry = Contacts(name=name,email=email,phone_num=phone,message=msg,date=datetime.now())
+        db.session.add(entry)
+        db.session.commit()
     return render_template("contact.html")
 
 app.run(debug=True)
