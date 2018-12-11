3c3f9baaa43e90074f17351e13ce9d1024dbba92
Karan Chaudhari <chaudharik353@gmail.com>
2018-12-11 14:01:21 +0530

    Add more posts and working prev and next buttons

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-import json,os
+import json, os, math
 
 with open('config.json','r') as f:
     params = json.load(f)['params']
@@ -35,8 +35,23 @@ class Contacts(db.Model):
 
 @app.route("/")
 def home():
-    posts = Posts.query.filter_by().all()
-    return render_template("index.html",params=params,posts=posts)
+    posts = Posts.query.filter_by().all()[::-1]
+    last = math.ceil(len(posts)/int(params['home_posts']))
+    page = request.args.get('page')
+    if not str(page).isnumeric():
+        page = 1
+    page = int(page)
+    posts = posts[(page-1)*int(params['home_posts']):(page-1)*int(params['home_posts'])+int(params['home_posts'])] 
+    if page == 1:
+        prev = "#"
+        next = "/?page=" + str(page+1)
+    elif page == last:
+        prev = "/?page=" + str(page-1)
+        next = "#"       
+    else:
+        prev = "/?page=" + str(page-1)
+        next = "/?page=" + str(page+1)    
+    return render_template("index.html",params=params,posts=posts,prev=prev,next=next)
 
 @app.route("/about")
 def about():
